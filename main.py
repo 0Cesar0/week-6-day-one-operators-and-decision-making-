c97698d8c59fc7ddc421493175e91d19eee53dd1
Cesar Diaz <113930497+0Cesar0@users.noreply.github.com>
2022-09-28 17:11:49 +0000

    review of .remove

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # min/max practice
 # Random in python
 # List comprehension
-
+print("Cesar Diaz")
 
 # review practice
 # Append the value of current to the end of the list seconds Please use the list.append() method to do that.
@@ -14,14 +14,19 @@
 
 seconds = [1.23, 1.45, 1.02]
 current = 1.11
-
+seconds.append(current)
+print(seconds)
 # Remove item 1.45 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
-
+seconds.remove(1.45)
+print(seconds)
 
 # Remove items 1.45, 1.02, and 1.11 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
-
+seconds.remove(1.45)
+seconds.remove(1.02)
+seconds.remove(1.11)
+print(seconds)
 
 ################################comparison operators#########################
 #remember....
